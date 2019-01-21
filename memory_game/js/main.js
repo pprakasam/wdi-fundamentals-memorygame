8644b357ba4dd14da80ad718600241b9bf48611c
Padma Sivaprakasam <padmaprakasam@gmail.com>
2019-01-20 19:35:53 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
 //console.log("Up and Running");
 
-var cardOne = 'King';
-var cardTwo = 'King';
-var CardThree = 'Queen';
-var cardFour = 'Queen';
-console.log("user flipped " + cardOne);
+var cards = ['queen', 'queen', 'king','king'];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match");
+	} else {
+		alert("Sorry, try again");
+	}
+
+}
